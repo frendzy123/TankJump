ac51fb327ee8a15c44892591e4c700fa5bc67988
frendzy123 <frendzy123@github.com>
2017-06-30 23:42:55 -0700

    First Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D body;
+
+	// Use this for initialization
+	void Start() 
+	{
+
+		body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+
+		Movement();
+	}
+
+	private void Movement()
+	{
+
+		if (Input.GetKeyDown (KeyCode.D))
+			body.velocity = Vector2.right;
+		
+		else if (Input.GetKeyUp (KeyCode.D))
+			body.velocity = Vector2.zero;
+
+		if (Input.GetKeyDown (KeyCode.A))
+			body.velocity = Vector2.left;
+
+		else if (Input.GetKeyUp (KeyCode.A))
+			body.velocity = Vector2.zero;
+	}
+}
---
+++ Assets/Scripts/TurretController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+
+		Aim();
+	}
+
+	// Function that rotates turret to face cursor.
+	void Aim()
+	{
+
+		Vector3 mousePos = Input.mousePosition; //Obtain mouse position
+		mousePos.z = 5f;
+		Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position); //Get coordinates of turret
+
+		mousePos.x = mousePos.x - objectPos.x;
+		mousePos.y = mousePos.y - objectPos.y;
+
+		float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg; //Calculate angle in rads using tan of x and y
+		transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+	}
+}
