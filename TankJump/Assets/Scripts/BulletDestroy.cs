ed2e364e4a44107015e1c211b8f5082921cf32af
frendzy123 <frendzy123@github.com>
2017-07-10 21:32:25 -0700

    Shooting Projectiles added

---
+++ TankJump/Assets/Scripts/BulletDestroy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDestroy : MonoBehaviour {
+
+	public float delay; // Duration the bullet exists in the game.
+
+	// Use this for initialization
+	void Start() 
+	{
+		
+		Destroy(this.gameObject, delay); // Destroy after the specified delay has passed
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		
+	}
+
+	// Function used to destroy when bullet collides with something.
+	void DestroyOnCollision()
+	{
+		
+		Destroy(this.gameObject);
+	}
+}
---
+++ TankJump/Assets/Scripts/Shooting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+	public GameObject projectile;
+	public float speed;
+
+	// Use this for initialization
+	void Start() 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+
+		Shoot();
+	}
+
+	void Shoot()
+	{
+
+		if(Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			Debug.Log(transform.position);
+			GameObject bullet = (GameObject) Instantiate(projectile, transform.position, transform.rotation); // Instantiates the bullet
+			Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), transform.parent.GetComponent<Collider2D>()); // Ignore collisions with turret
+			Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), transform.parent.parent.GetComponent<Collider2D>()); // Ignore collisions with tank
+			Rigidbody2D bulletRigid = (Rigidbody2D) bullet.GetComponent<Rigidbody2D>(); // Get rigidbody from the bullet.
+			bulletRigid.velocity = transform.right * speed; // Add a velocity towards the direction of the shooting points
+		}
+	}
+}
