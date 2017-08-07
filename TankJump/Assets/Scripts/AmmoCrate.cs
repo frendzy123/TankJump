9b8f50c4c0a1393d18e9e3e00bdc831e01607b89
frendzy123 <frendzy123@github.com>
2017-08-06 20:47:32 -0700

    Created prefabs for ammo crates and health packs

---
+++ TankJump/Assets/Scripts/AmmoCrate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoCrate : MonoBehaviour {
+
+	public int ammoCount;
+
+	// Use this for initialization
+	void Start () {
+
+		ammoCount = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+
+		if(coll.gameObject.tag == "Player") 
+		{
+
+			coll.gameObject.GetComponent<PlayerController>().ammo += ammoCount;
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ TankJump/Assets/Scripts/HealthPack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPack : MonoBehaviour {
+
+	public int healthCount;
+
+	// Use this for initialization
+	void Start () {
+
+		healthCount = 50;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+
+		if(coll.gameObject.tag == "Player") 
+		{
+
+			coll.gameObject.GetComponent<PlayerController>().health += healthCount;
+			Destroy(this.gameObject);
+		}
+	}
+}
