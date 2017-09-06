75e3ef806a53b7c32146ea5edb139a68b49205aa
frendzy123 <frendzy123@github.com>
2017-09-05 19:40:59 -0700

    Prefabs for different environment types

---
+++ TankJump/Assets/Scripts/DestructableEnvironment.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructableEnvironment : MonoBehaviour {
+
+	public int delay;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+
+		if (coll.gameObject.tag == "Explosion") 
+		{
+
+			Destroy (this.gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+
+		if (coll.gameObject.tag == "Player") 
+		{
+
+			Destroy (this.gameObject, delay);
+		}
+	}
+}
---
+++ TankJump/Assets/Scripts/InvisGround.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisGround : MonoBehaviour {
+
+	private SpriteRenderer renderer;
+
+	// Use this for initialization
+	void Start () {
+
+		renderer = this.gameObject.GetComponent<SpriteRenderer>();
+		renderer.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+
+		if (coll.gameObject.tag == "Explosion") 
+		{
+
+			renderer.enabled = true;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+
+		if (coll.gameObject.tag == "Player") 
+		{
+
+			renderer.enabled = true;
+		}
+	}
+}
