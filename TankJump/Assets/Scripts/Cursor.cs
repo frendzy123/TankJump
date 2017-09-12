820b5c9af61e33ff6a92c74fa58b6538916ea9bb
Xavier Chua <xavierc.1998@gmail.com>
2017-09-12 12:38:58 -0700

    Added crosshair + script

---
+++ TankJump/Assets/Scripts/Cursor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+
+		this.transform.position = Vector3.zero + mousePos;
+	}
+}
