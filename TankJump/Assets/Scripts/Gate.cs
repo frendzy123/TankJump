3e852f22bab6594ad120119e498e98909bb44712
frendzy123 <frendzy123@github.com>
2017-09-13 21:28:13 -0700

    Adding Switch and Gate Prefabs

---
+++ TankJump/Assets/Scripts/Gate.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour {
+
+	private GameObject[] switches;
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+
+		count = 0;
+		switches = GameObject.FindGameObjectsWithTag("Switch");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		foreach(GameObject obj in switches)
+		{
+
+			if (!obj.GetComponent<Switch> ().active) 
+			{
+
+				break;
+			}
+
+			count += 1;
+		}
+
+		if (count == switches.Length) {
+
+			Destroy (this.gameObject);
+		} 
+
+		else 
+		{
+
+			count = 0;
+		}
+	}
+}
